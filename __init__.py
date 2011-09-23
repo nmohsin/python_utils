252c0df82fc4f1ac75a14327070c8f51103cc0d0
Nadeem Mohsin <abstractwhiz@gmail.com>
2011-09-23 00:03:00 -0400

    Updated docstring in __init__.py.

--- __init__.py
+++ __init__.py
@@ -3,4 +3,8 @@
 
 The following types of functionality are found here:
 - files.py: Manipulating and extracting information from file/directory names.
+- extraction.py: Extracting text/tokens/tagged tokens from files.
+- sorting.py: Common types of sorting.
+- decorators.py: Function decorators I often use.
+- db.py: Functions for programatically manipulating databases.
 """
